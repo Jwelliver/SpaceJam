27ca0185d258e45f57a67011ee133fc68502c214
Josh Welliver <joshwelliver@gmail.com>
2024-06-14 20:06:35 -0700

    Update PlayerManager.cs

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -29,7 +29,7 @@ public class PlayerManager : MonoBehaviour
     }
 
     void OnPlayerLeave(PlayerInput playerInput) {
-        if(playerInputManager!=null && playerInputManager.playerCount==0) {
+        if(prejoinUI!=null && playerInputManager.playerCount==0) {
             prejoinUI.SetActive(true);
         }
     }
