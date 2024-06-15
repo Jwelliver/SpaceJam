3a524f43dd7c680b5db5ec3b9e1b1eb223caf5e3
Josh Welliver <joshwelliver@gmail.com>
2024-06-14 18:32:05 -0700

    Add PreJoinUI
    e.g. "Press Start to Join"

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    public GameObject prejoinUI;
     private PlayerInputManager playerInputManager;
 
     Vector3 playerSpawnPoint = Vector3.zero;
@@ -21,13 +22,16 @@ public class PlayerManager : MonoBehaviour
     }
 
     void OnPlayerJoin(PlayerInput playerInput) {
+        if(prejoinUI.activeSelf) { prejoinUI.SetActive(false);}
         playerInput.transform.root.position = playerSpawnPoint;
         playerSpawnPoint+=new Vector3(10,0,0);
         Debug.Log("Player Joined"+playerInput.name);
     }
 
     void OnPlayerLeave(PlayerInput playerInput) {
-
+        if(playerInputManager.playerCount==0) {
+            prejoinUI.SetActive(true);
+        }
     }
 
 
