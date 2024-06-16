323acd9f20d876347d3595a75150040d14f64e1d
Josh Welliver <joshwelliver@gmail.com>
2024-06-15 20:31:42 -0700

    Update PlayerManager.cs
    -Added Player list
    -made playerInputManager reference public

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,12 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
     public GameObject prejoinUI;
-    private PlayerInputManager playerInputManager;
+
+    private PlayerInputManager _playerInputManager;
+    public PlayerInputManager playerInputManager {
+         get {
+            if(_playerInputManager ==null) {
+                _playerInputManager=FindObjectOfType<PlayerInputManager>();
+            }
+            return _playerInputManager;
+        }
+    }
+
+    public List<Player> players = new List<Player>();
 
     Vector3 playerSpawnPoint = Vector3.zero;
 
     void Start() {
-        playerInputManager = ProjectRefs.playerInputManager;
+        // playerInputManager = ProjectRefs.playerInputManager;
         playerInputManager.onPlayerJoined += OnPlayerJoin;
         playerInputManager.onPlayerLeft += OnPlayerLeave;
     }
@@ -22,6 +33,8 @@ public class PlayerManager : MonoBehaviour
     }
 
     void OnPlayerJoin(PlayerInput playerInput) {
+        // Make sure preJoinUI is hidden
+        players.Add(playerInput.transform.GetComponentInParent<Player>());
         if(prejoinUI.activeSelf) { prejoinUI.SetActive(false);}
         playerInput.transform.root.position = playerSpawnPoint;
         playerSpawnPoint+=new Vector3(10,0,0);
@@ -29,6 +42,8 @@ public class PlayerManager : MonoBehaviour
     }
 
     void OnPlayerLeave(PlayerInput playerInput) {
+
+        players.Remove(playerInput.transform.GetComponentInParent<Player>()); //TODO: Not sure if this will work since the player object might be destroyed at this point? Try using a hashmap with player identifier (look into whether playerIndex changes)
         if(prejoinUI!=null && playerInputManager.playerCount==0) {
             prejoinUI.SetActive(true);
         }
