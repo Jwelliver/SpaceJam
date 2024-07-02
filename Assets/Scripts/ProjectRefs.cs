db8b2a96b85826a9b766cf0631366cd50b54c69e
Josh Welliver <joshwelliver@gmail.com>
2024-07-02 15:36:36 -0700

    Update ProjectRefs.cs

--- Assets/Scripts/ProjectRefs.cs
+++ Assets/Scripts/ProjectRefs.cs
@@ -6,16 +6,24 @@ public class ProjectRefs : MonoBehaviour
     public static ProjectRefs Instance;
     [SerializeField] PlayerInputManager _playerInputManager;
     [SerializeField] AmmoRef _ammoRef;
+    [SerializeField] ShipCustomizationStore _shipCustomizationStore;
     public static AmmoRef ammoRef {get {return ProjectRefs.Instance._ammoRef;}}
     public static PlayerInputManager playerInputManager {get {return ProjectRefs.Instance._playerInputManager;}}
+    public static ShipCustomizationStore shipCustomizationStore {get {return ProjectRefs.Instance._shipCustomizationStore;}}
     
 
     void Awake() {
-        if(ProjectRefs.Instance!=null) {
+        if(ProjectRefs.Instance!=null && ProjectRefs.Instance!=this) {
             Destroy(this);
         } else {
             ProjectRefs.Instance = this;
         }
+    }
 
+
+    void OnDisable() {
+        if(ProjectRefs.Instance==this) {
+            Destroy(this);
+        }
     }
 }
